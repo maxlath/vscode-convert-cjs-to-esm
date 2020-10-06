3dcb310d6e6f86136c438744f29de1dad3fd8997
maxlath <k@maxlath.eu>
2020-10-06 21:05:51 +0200

    fix case where a wrapper object with a "default" attribute was used

--- convert.js
+++ convert.js
@@ -20,9 +20,8 @@ const convert = async (textEditor, transformModuleName) => {
   const { line: cursorLineNum } = textEditor._selections[0].active
   const lines = textEditor._documentData._lines
   const requireLine = lines[cursorLineNum]
-  console.log('requireLine', requireLine)
   const requireLineLength = requireLine.length
-  const requireFilepathMatch = requireLine.match(/: require\(('|")(.*)('|")\),?$/)
+  const requireFilepathMatch = requireLine.match(/require\(('|")([^)]*)('|")\)/)
   if (requireFilepathMatch == null) {
     console.warn('[vscode.window.activeTextEditor] require line not found', { cursorLineNum, line: requireLine })
     return
@@ -32,14 +31,14 @@ const convert = async (textEditor, transformModuleName) => {
   console.log('moduleFilename', moduleFilename)
   console.log('moduleFilepath', moduleFilepath)
   const moduleName = transformModuleName(moduleFilename)
-  console.log('moduleName', moduleName)
 
   const requireStart = new vscode.Position(cursorLineNum, 0)
   const requireEnd = new vscode.Position(cursorLineNum, requireLineLength)
   const requireRange = new vscode.Range(requireStart, requireEnd)
-  console.log('requireRange', requireRange)
 
-  const updatedRequireLine = requireLine.replace(/: require.*$/, `: ${moduleName},`)
+  const updatedRequireLine = requireLine
+    .replace(/require\([^)]*\)/, moduleName)
+    .replace('.default', '')
 
   const { index, isEmptyLine } = getIndexOfFirstListBelowImports(lines)
   const newImportLinePosition = new vscode.Position(Math.max(index - 1, 0), 0)
@@ -47,7 +46,6 @@ const convert = async (textEditor, transformModuleName) => {
   if (!isEmptyLine) addedImportLine += '\n'
 
   vscode.window.activeTextEditor.edit(editBuilder => {
-    console.log('editBuilder', editBuilder)
     editBuilder.replace(requireRange, updatedRequireLine)
     editBuilder.insert(newImportLinePosition, addedImportLine)
   })
